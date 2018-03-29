fdf17c9b18863fc3ce597a8e76ddf5ba9829cdeb
zhuluxi <598416667@qq.com>
2018-03-29 22:49:50 +0800

    画图

--- summary/New Group/Quartzs_2D/drawView.m
+++ summary/New Group/Quartzs_2D/drawView.m
@@ -10,11 +10,34 @@
 
 @implementation drawView
 
+/*
+ 1.UIImage对象直接draw      drawInRect
+ 2.label有文本直接draw      drawTextInRect
+ */
 -(void)drawRect:(CGRect)rect
 {
     [super drawRect:rect];
 
-    NSLog(@"drawView----drawRect方法-");
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetStrokeColorWithColor(context, [[UIColor redColor] colorWithAlphaComponent:1.0].CGColor);
+    CGContextStrokeRectWithWidth(context, self.bounds, 1);
 
 }
+
+
+//UIImage对象直接画
+- (void)drawRectWithImageObj{
+    UIImage *iamge = [UIImage imageNamed:@"22.jpeg"];
+    [iamge drawInRect:self.bounds];
+}
+
+//label有文本直接draw
+- (void)drawRectWithLableObj{
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(0, 20, 100, 20)];
+    label.backgroundColor = [UIColor redColor];
+    label.text = @"这是画上去的";
+    label.font = [UIFont systemFontOfSize:9];
+    [label drawTextInRect:label.frame];
+}
+
 @end
