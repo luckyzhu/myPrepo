5cd62c2d326b7cc2f06b7272b089d4a2fd6ed3bf
zhuluxi <598416667@qq.com>
2018-03-29 16:28:09 +0800

    添加.gitignore文件

---
+++ summary/New Group/test02/22ViewController.h
@@ -0,0 +1,13 @@
+//
+//  22ViewController.h
+//  summary
+//
+//  Created by NewBoy on 2018/3/29.
+//  Copyright © 2018年 LX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface _2ViewController : UIViewController
+
+@end
---
+++ summary/New Group/test02/22ViewController.m
@@ -0,0 +1,38 @@
+
+//
+//  22ViewController.m
+//  summary
+//
+//  Created by NewBoy on 2018/3/29.
+//  Copyright © 2018年 LX. All rights reserved.
+//
+
+#import "22ViewController.h"
+
+@interface _2ViewController ()
+
+@end
+
+@implementation _2ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
