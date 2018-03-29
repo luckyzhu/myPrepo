a4a03d5c967624779470c7a2f2b9c2477e7767c3
zhuluxi <598416667@qq.com>
2018-03-29 16:02:23 +0800

    解决冲突

---
+++ summary/New Group/git/gitViewController.h
@@ -0,0 +1,17 @@
+//
+//  gitViewController.h
+//  summary
+//
+//  Created by NewBoy on 2018/3/29.
+//  Copyright © 2018年 LX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gitViewController : UIViewController
+
+/*
+ 1.设置gitignore 全局忽略DS_Store.等文件
+
+ */
+@end
